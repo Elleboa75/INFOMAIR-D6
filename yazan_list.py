ef7ebdf2ef4f885a6ea94f60498dfa05dab5cb82
ymous <yazanmousa8@gmail.com>
2025-09-09 14:56:16 +0200

    Add list of 4

---
+++ yazan_list.py
@@ -0,0 +1,31 @@
+
+
+deny = {
+    "not any more",
+    "i dont want",
+    "wrong",
+    "not european fusion",
+    "dont want that",
+    "can you change romanian food to something else",
+    "that is not important",
+}
+
+hello = {
+    "hi",
+    "hello",
+    "halo",
+}
+
+inform = {
+    "okay im looking for moderate price range",
+    "any area",
+    "i dont care",
+    "i would like a cheap restaurant in the north part of town",
+}
+
+negate = {
+    "not any more",
+    "no",
+}
+
+
