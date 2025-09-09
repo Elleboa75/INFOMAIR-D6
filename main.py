455be15fe5183c9caac44de3358f1e498c4f149a
zvJustin <justin013nl@gmail.com>
2025-09-09 14:27:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,34 @@
 import pandas as pd
-dialog_act = []
-utterance_content = []
 
-with open("dialog_acts.dat") as f:
-    for line in f:
-        split_line = line.split(' ')
-        act = split_line[0]
-        utterance =  ' '.join(split_line[1:])
-        dialog_act.append(act)
-        utterance_content.append(utterance)
-        
-        
-    f.close()
+
+def format_data(data):
+    dialog_act  = []
+    utterance_content = []
+    with open(data) as f:
+        for line in f:
+            split_line = line.split(' ')
+            act = split_line[0]
+            utterance =  ' '.join(split_line[1:])
+            dialog_act.append(act)
+            utterance_content.append(utterance)
+
+
+        f.close()
+    return dialog_act, utterance_content
+
+    
+def baseline_1(labels):
+    return Counter(labels).most_common()[0][0]
+
+
     
     
 
     
 
 if __name__ == '__main__':
-    pass
+    labels, text = format_data("dialog_acts.dat" )
+    print(baseline_1(labels))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
