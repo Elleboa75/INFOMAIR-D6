33b240660dec65731108502a2aaf7770f93e364d
zvJustin <justin013nl@gmail.com>
2025-09-09 14:15:37 +0200

    Update date splitting

--- main.py
+++ main.py
@@ -4,16 +4,15 @@ utterance_content = []
 
 with open("all_dialogs.txt") as f:
     for line in f:
-        split_line = line.split(':')
-        if len(split_line) > 1:
-            act = split_line[0]
-            utterance = split_line[1]
-            dialog_act.append(act)
-            utterance_content.append(utterance)
-
+        split_line = line.split(' ')
+        act = split_line[0]
+        utterance =  ' '.join(split_line[1:])
+        dialog_act.append(act)
+        utterance_content.append(utterance)
+        
+        
     f.close()
-
-dialog_act
+    
 
 if __name__ == '__main__':
     pass
