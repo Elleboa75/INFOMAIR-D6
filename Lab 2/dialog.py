7a2f480ad05edd91787211dca4d91496a80229f0
zvJustin <justin013nl@gmail.com>
2025-09-28 20:26:02 +0200

    Added NN model as dialog_act predictor

--- Lab 2/dialog.py
+++ Lab 2/dialog.py
@@ -1,7 +1,10 @@
 import pandas as pd
 from utils.dialog_manager import DialogManager
-
+from machine_learning import MachineModelOne
+import tensorflow as tf
 if __name__ == "__main__":
     df = pd.read_csv("data/restaurant_info_additional_data.csv")
-    dm = DialogManager(df, config_path = "utils/dialog_config.json")   # pass dataframe (and optionally config path, model)
+    NNmodel = MachineModelOne(dataset_location="dialog_acts.dat",
+                              model=tf.keras.models.load_model("models/NN_model.keras"))
+    dm = DialogManager(df, model = NNmodel, config_path = "utils/dialog_config.json")   # pass dataframe (and optionally config path, model)
     dm.run_dialog()
